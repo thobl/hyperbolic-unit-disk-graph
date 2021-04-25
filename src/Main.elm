2f3a90ac54807fcd6a157f843bcc5864909d13f6
Thomas Bläsius <thomas.blaesius@kit.edu>
2021-04-25 18:10:04 +0200

    minor bugfix for the clique (too high avgDeg for number of vertices)

--- src/Main.elm
+++ src/Main.elm
@@ -204,7 +204,7 @@ updateThresholdR model =
                     pairDist pair
 
                 Nothing ->
-                    0
+                    2 * model.groundSpaceR
     in
     { model | thresholdRadius = thresholdR }
 
@@ -229,13 +229,13 @@ update msg model =
             { model | pointsVirt = newPoints } |> updatePoints |> noCmd
 
         InputNrVertices n ->
-            { model | n = round n } |> updatePoints |> noCmd
+            { model | n = max (round n) (ceiling model.avgDeg + 1) } |> updatePoints |> noCmd
 
         InputCanvasSize size ->
             { model | canvasSize = round size } |> updatePoints |> noCmd
 
         InputAvgDeg avgDeg ->
-            { model | avgDeg = avgDeg } |> updateThresholdR |> noCmd
+            { model | avgDeg = min avgDeg (toFloat model.n - 1) } |> updateThresholdR |> noCmd
 
         InputGroundSpaceR x ->
             { model | groundSpaceR = inputToGroundSpaceR x } |> updatePoints |> noCmd
